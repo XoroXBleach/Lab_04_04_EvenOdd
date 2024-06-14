276aef5dedf3a21881ddc80bb53ce5e1623797fc
xoroxbleach <morinxander04@gmail.com>
2024-06-13 23:54:30 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    int Number = 2;
+    double NumberMod = Number % 2;
+    if (NumberMod == 0) {
+        System.out.print("The number examined is even");}
+    else {
+        System.out.print("The number examined is odd");}
+    }
+
+        }
